fbfe0404efaa4fe9f2ebb3a7048f7f064afc80fa
Zachary King <kingzach77@gmail.com>
2017-07-11 21:07:38 -0500

    simple test case

--- mysite/pynny/tests.py
+++ mysite/pynny/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        '''Tests that 1 + 1 = 2'''
+        self.assertEqual(1 + 1, 2)
