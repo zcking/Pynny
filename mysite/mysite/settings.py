ffe519244593b5d36066b9e432cfeb4f6f497976
Zachary King <kingzach77@gmail.com>
2017-09-04 21:31:59 -0500

    travis fix

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'OPTIONS': {
-            'read_default_file': 'my.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'pynnydb.sqlite3'),
     }
 }
 
