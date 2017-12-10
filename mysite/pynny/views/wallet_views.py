f67b17bb8c5d332cb627cac81fdfda98295883e2
zking <zking@firstorion.com>
2017-12-10 15:21:06 -0600

    refactored wallet views to class-based-views

--- mysite/pynny/views/wallet_views.py
+++ mysite/pynny/views/wallet_views.py
@@ -1,109 +1,146 @@
 #!/usr/bin/env python3
-'''
+"""
 File: wallet_views.py
 Author: Zachary King
 
 Implements views/handlers for Wallet-related requests
-'''
+"""
 
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render
 from datetime import date
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
 from ..models import Wallet, Budget, Transaction
 
 
-@login_required(login_url='/pynny/login')
-def wallets(request):
-    '''Display Wallets for a user'''
-    # GET = display user's wallets
-    if request.method == 'GET':
-        data = dict()
+class WalletsView(LoginRequiredMixin, View):
+    """Handles requests to the wallets resources"""
+    current_tab = 'wallets'
 
-        # Get the wallets for this user
-        data['wallets'] = Wallet.objects.filter(user=request.user)
+    @staticmethod
+    def wallets_with_alerts(request, alerts, status=200):
+        context = {
+            'current_tab': WalletsView.current_tab,
+            'wallets': Wallet.objects.filter(user=request.user),
+            'alerts': alerts
+        }
+        return render(request, 'pynny/wallets/wallets.html', context=context, status=status)
 
-        data['current_tab'] = 'wallets'
-        return render(request, 'pynny/wallets/wallets.html', context=data)
-    # POST = create a new Wallet
-    elif request.method == 'POST':
-        # Get the form data from the request
-        name = request.POST['name']
-        start_balance = 0
-        start_balance = float(request.POST['balance'])
+    @staticmethod
+    def get(request):
+        return WalletsView.wallets_with_alerts(request, {})
 
-        # Check if the wallet name exists already
-        if Wallet.objects.filter(user=request.user, name=name):
-            data = {'alerts': {'errors': ['A wallet already exists with that name']}}
-            data['current_tab'] = 'wallets'
-            return render(request, 'pynny/wallets/new_wallet.html', context=data)
+    @staticmethod
+    def post(request):
+        """Parse form-data and create a new Wallet"""
+        name = request.POST.get('name', None)
+        start_balance = request.POST.get('balance', '0.0')
+
+        if name is None:
+            return WalletsView.wallets_with_alerts(
+                request,
+                {'errors': ['You must give a unique name for your wallet.']},
+                status=400
+            )
+        else:
+            if Wallet.objects.filter(user=request.user, name=name):
+                return WalletsView.wallets_with_alerts(
+                    request,
+                    {'errors': ['That name already exists. Please name your wallet uniquely.']},
+                    status=400
+                )
+
+        try:
+            start_balance = float(start_balance)
+        except ValueError:
+            start_balance = 0.0
 
         # Create the new Wallet
         Wallet(name=name, balance=start_balance, user=request.user).save()
-        data = {'alerts': {'success': ['<strong>Done!</strong> New wallet created successfully!']}}
-        data['current_tab'] = 'wallets'
-        data['wallets'] = Wallet.objects.filter(user=request.user)
-        return render(request, 'pynny/wallets/wallets.html', context=data, status=201)
+        return WalletsView.wallets_with_alerts(
+            request,
+            {'success': ['<em>{0}</em> wallet created successfully.'.format(name)]},
+            status=201
+        )
 
 
-@login_required(login_url='/pynny/login')
-def new_wallet(request):
-    '''Display form for creating a new wallet'''
-    data = dict()
-    data['current_tab'] = 'wallets'
-    return render(request, 'pynny/wallets/new_wallet.html', context=data)
+class SingleWalletView(LoginRequiredMixin, View):
+    """Handles requests to a single wallet resource"""
+    current_tab = 'wallets'
 
+    @staticmethod
+    def pre_process(request, wallet_id):
+        """Validation and checking"""
+        try:
+            wallet = Wallet.objects.get(id=wallet_id)
+        except Wallet.DoesNotExist:
+            return False, WalletsView.wallets_with_alerts(
+                request,
+                {'errors': ['That wallet does not exist.']},
+                status=404
+            )
 
-@login_required(login_url='/pynny/login')
-def one_wallet(request, wallet_id):
-    '''Handles requests to a specific wallet'''
-    data = dict()
-    data['current_tab'] = 'wallets'
+        if wallet.user != request.user:
+            return False, WalletsView.wallets_with_alerts(
+                request,
+                {'errors': ['That wallet does not exist.']},
+                status=403
+            )
 
-    # Check if the wallet is owned by the logged in user
-    try:
-        wallet = Wallet.objects.get(id=wallet_id)
-    except Wallet.DoesNotExist:
-        # DNE
-        data['wallets'] = Wallet.objects.filter(user=request.user)
-        data['alerts'] = {'errors': ['<strong>Oh snap!</strong> That wallet does not exist.']}
-        return render(request, 'pynny/wallets/wallets.html', context=data, status=404)
+        return True, wallet
 
-    if wallet.user != request.user:
-        data['wallets'] = Wallet.objects.filter(user=request.user)
-        data['alerts'] = {'errors': ['<strong>Oh snap!</strong> That wallet does not exist.']}
-        return render(request, 'pynny/wallets/wallets.html', context=data, status=403)
+    @staticmethod
+    def get(request, wallet_id):
+        """Get the single wallet's data and render it"""
+        success, resp = SingleWalletView.pre_process(request, wallet_id)
+        if not success:
+            return resp
+        else:
+            wallet = resp
+            context = {
+                'current_tab': SingleWalletView.current_tab,
+                'wallet': wallet,
+                'budgets': Budget.objects.filter(wallet=wallet, month__contains=date.strftime(date.today(), '%Y-%m')),
+                'transactions': Transaction.objects.filter(wallet=wallet).order_by('-created_time')
+            }
+            return render(request, 'pynny/wallets/one_wallet.html', context=context)
 
-    if request.method == 'POST':
+    @staticmethod
+    def post(request, wallet_id):
+        success, resp = SingleWalletView.pre_process(request, wallet_id)
+        if not success:
+            return resp
+        else:
+            wallet = resp
+
         # What action?
-        action = request.POST['action'].lower()
+        action = request.POST.get('action', '').lower()
 
         if action == 'delete':
             # Delete the wallet
             wallet.delete()
 
             # And return them to the wallets page
-            data['wallets'] = Wallet.objects.filter(user=request.user)
-            data['alerts'] = {'info': ['<strong>Done!</strong> Your <em>' + wallet.name + '</em> wallet was successfully deleted']}
-            return render(request, 'pynny/wallets/wallets.html', context=data)
-        elif action == 'edit':
-            # Render the edit_wallet view
-            data['wallet'] = wallet
-            return render(request, 'pynny/wallets/edit_wallet.html', context=data)
+            return WalletsView.wallets_with_alerts(
+                request,
+                {'success': ['<em>{}</em> wallet deleted successfully.'.format(wallet.name)]}
+            )
         elif action == 'edit_complete':
             # Get the form data from the request
-            _name = request.POST['name']
+            _name = request.POST.get('name', None)
+            if _name is None or len(Wallet.objects.filter(user=request.user, name=_name)) > 0:
+                return WalletsView.wallets_with_alerts(
+                    request,
+                    {'errors': ['Please choose a unique, nonempty name for your wallet.']},
+                    status=400
+                )
 
             # Edit the Wallet
             wallet.name = _name
             wallet.save()
 
-            data['alerts'] = {'success': ['<strong>Done!</strong> Wallet updated successfully!']}
-            data['wallets'] = Wallet.objects.filter(user=request.user)
-            return render(request, 'pynny/wallets/wallets.html', context=data)
-    elif request.method == 'GET':
-        # Show the specific Wallet data
-        data['wallet'] = wallet
-        data['budgets'] = Budget.objects.filter(wallet=wallet, month__contains=date.strftime(date.today(), '%Y-%m'))
-        data['transactions'] = Transaction.objects.filter(wallet=wallet).order_by('-created_time')
-        return render(request, 'pynny/wallets/one_wallet.html', context=data)
+            return WalletsView.wallets_with_alerts(
+                request,
+                {'success': ['Wallet updated successfully']}
+            )
